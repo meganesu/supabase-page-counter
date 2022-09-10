38c8b86ddd7a4b4bccd95563e220db5e482436f3
Megan Sullivan <meganesulli@gmail.com>
2022-09-10 12:23:24 -0700

    feat: call supabase API to load & store page views
    - shows network requests to supabase API in the browser, which means users could access API directly (not ideal). to be fixed in future commit.

--- src/components/PageCounter.js
+++ src/components/PageCounter.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import supabase from '../utils/supabase'
 
 const PageViewCounter = ({url}) => {
-  const numberOfViews = 0
+  const [numberOfViews, setNumberOfViews] = useState()
+
+  useEffect(() => {
+    const getPageViewCount = async () => {
+      const response = await supabase
+        .from('page_views')
+        .select('*', { count: 'exact' })
+        .eq('page_url', url)
+      console.log(response)
+      return response
+    }
+
+    const addNewPageView = async () => {
+      return await supabase
+        .from('page_views')
+        .insert([
+          {page_url: url}
+        ])
+    }
+
+    getPageViewCount()
+      .then(({ count, error }) => {
+        if (error) {
+          throw new Error(error)
+        }
+
+        console.log({ count, error })
+
+        if (!count) {
+          console.log('no matching row for this URL:', url)
+          setNumberOfViews(1)
+        }
+        else {
+          setNumberOfViews(count + 1)
+        }
+
+        return addNewPageView()
+      })
+      .then(({error}) => {
+        if (error) {
+          throw new Error(error)
+        }
+      })
+
+
+  }, [url])
+
 
   return (
     <p>This page has been viewed {numberOfViews} times.</p>
