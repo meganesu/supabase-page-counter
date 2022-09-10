8e718cc507d07ac31459cea8b65ccc6311e75392
Megan Sullivan <meganesulli@gmail.com>
2022-09-10 15:08:16 -0700

    test: add serverless function to see if it will work on github pages

--- src/pages/1.js
+++ src/pages/1.js
@@ -2,9 +2,31 @@ import * as React from "react"
 import Layout from "../components/Layout"
 
 const IndexPage = ({ location }) => {
+  const [ response, setResponse ] = React.useState()
+
+  const testServerlessFunction = async () => {
+    return await window.fetch('/api/hello-world', {
+      method: `GET`,
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+    .then(res => res.json())
+  }
+
+  React.useEffect(() => {
+    testServerlessFunction()
+      .then(response => {
+        setResponse(response)
+        console.log('SERVERLESS FUNCTION RESPONSE', response)
+      })
+  }, [])
+
+
   return (
     <Layout location={location}>
-      This is page 1.
+      <p>This is page 1.</p>
+      <p>{JSON.stringify(response)}</p>
     </Layout>
   )
 }
